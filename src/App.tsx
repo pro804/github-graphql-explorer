075cbf46f73cd6ee9071c380be7815f02e0e6cc6
pro804 <petsoulis.appdev@gmail.com>
2025-11-06 14:31:28 +0200

    feat: integrate GET_USER GraphQL query into UserProfile component - Updated `UserProfile.tsx` to fetch GitHub user data using Apollo `useQuery` hook:   - Executed `GET_USER` query with dynamic `userName` variable.   - Added typed query response with `UserData` for strong type safety. - Implemented conditional UI states:   - Displayed loading message during data fetch.   - Handled and displayed GraphQL errors gracefully.   - Rendered fallback message when user data is not found. - Destructured key user fields (`avatarUrl`, `name`, `bio`, `followers`, `following`, `gists`, `repositories`, `url`) for future display components. - Verified live data fetch and API connection functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SearchForm from "./components/form/SearchForm";
 import UserProfile from "./components/user/userProfile";
 
 const App = () => {
-  const [userName, setUserName] = useState("quincylarson");
+  const [userName, setUserName] = useState("pro804");
 
   return (
     <main className="mx-auto max-w-6xl px-8 py-20">
--- src/components/user/userProfile.tsx
+++ src/components/user/userProfile.tsx
@@ -1,8 +1,40 @@
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "@/queries";
+import { UserData } from "@/types";
+
 type UserProfileProps = {
   userName: string;
 };
 
 const UserProfile = ({ userName }: UserProfileProps) => {
-  return <h1 className="text-2xl font-bold">{userName}</h1>;
+  const { data, loading, error } = useQuery<UserData>(GET_USER, {
+    variables: { login: userName },
+  });
+
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+  if (error) {
+    return <h2 className="text-xl">{error.message}</h2>;
+  }
+
+  if (!data) {
+    return <h2 className="text-xl capitalize">user not found.</h2>;
+  }
+  const {
+    avatarUrl,
+    name,
+    bio,
+    followers,
+    following,
+    gists,
+    repositories,
+    url,
+  } = data.user;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">{bio}</h1>
+    </div>
+  );
 };
 export default UserProfile;
